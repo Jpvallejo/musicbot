cd6c9e3bfc4c15e90ca77097d81e8100e3cd0fdc
Juan Pablo Vallejo <jvallejo@solstice.com>
2021-09-14 17:11:28 -0300

    Add port

--- src/index.js
+++ src/index.js
@@ -92,3 +92,9 @@ async function execute(message, id) {
     }
 }
 client.login(token);
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
