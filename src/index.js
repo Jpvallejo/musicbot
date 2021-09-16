d73ab02202db02f0f9a390acac30466316d07049
Juan Pablo Vallejo <vallejojuanp@gmail.com>
2021-09-16 00:26:23 -0300

    Add dynamic method calls

--- src/index.js
+++ src/index.js
@@ -1,114 +1,115 @@
-const Discord = require("discord.js");
-require('dotenv').config()
-const { default: PlayService } = require("./services/playService");
-const Distube = require("distube");
-
-const prefix = process.env.PREFIX;
-const token = process.env.TOKEN;
-
-const client = new Discord.Client();
-const distube = new Distube(client, { searchSongs: false, emitNewSongOnly: true });
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var _this = this;
+var acceptedMethods = require("../src/configs/config.json").acceptedMethods;
+var Discord = require("discord.js");
+require("dotenv").config();
+var PlayService = require("./services/playService")["default"];
+var Distube = require("distube");
+var prefix = process.env.PREFIX;
+var token = process.env.TOKEN;
+var client = new Discord.Client();
+var distube = new Distube(client, {
+    searchSongs: false,
+    emitNewSongOnly: true
+});
 PlayService.setDistube(distube);
-
-const queue = new Map();
-
-client.once("ready", () => {
+var queue = new Map();
+client.once("ready", function () {
     console.log("Ready!");
 });
-
-client.once("reconnecting", () => {
+client.once("reconnecting", function () {
     console.log("Reconnecting!");
 });
-
-client.once("disconnect", () => {
+client.once("disconnect", function () {
     console.log("Disconnect!");
 });
-
-client.on("message", async message => {
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
-
-    const args = message.content.split(" ");
-
-    const cmd = args[0].substr(1, args[0].length);
-    console.log(cmd);
-    switch (cmd) {
-        case 'play':
-        case 'p':
-            execute(message, message.guild.id);
-            break;
-        case 'skip':
-        case 's':
-            PlayService.skip(message);
-            break;
-        case 'stop':
-            PlayService.stop(message);
-            break;
-        case 'loop':
-            PlayService.loop(message, args[1]);
-            break;
-        case 'queue':
-        case 'q':
-            PlayService.queue(message);
-            break;
-        case 'fastforward':
-        case 'ff':
-            PlayService.jumpSong(message, Number(args[1]));
-            break;
-        case 'rewind':
-            PlayService.jumpSong(message, -1 * Number(args[1]));
-            break;
-        case 'pause':
-            PlayService.pause(message);
-            break;
-        case 'shuffle':
-            PlayService.shuffle(message);
-            break;
-        default:
+client.on("message", function (message) { return __awaiter(_this, void 0, void 0, function () {
+    var args, cmd, cmdString;
+    return __generator(this, function (_a) {
+        if (message.author.bot)
+            return [2 /*return*/];
+        if (!message.content.startsWith(prefix))
+            return [2 /*return*/];
+        if (!checkPermissions(message))
+            return [2 /*return*/];
+        console.log("adsaasddda");
+        args = message.content.split(" ");
+        cmd = args[0].substr(1, args[0].length);
+        args = args.splice(1, args.length);
+        cmdString = args.join(" ");
+        if (!acceptedMethods.includes(cmd)) {
             message.channel.send("You need to enter a valid command!");
-            break;
-    }
-});
-
-async function execute(message, id) {
-    var args = message.content.split(" ");
-    args = args.splice(1, args.length);
-    const requestedSong = args.join(" ");
-
-    const voiceChannel = message.member.voice.channel;
-    if (!voiceChannel)
-        return message.channel.send(
-            "You need to be in a voice channel to play music!"
-        );
-    const permissions = voiceChannel.permissionsFor(message.client.user);
-    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-        return message.channel.send(
-            "I need the permissions to join and speak in your voice channel!"
-        );
-    }
-    QueueService.setChannels(id, message.channel, voiceChannel);
-    PlayService.play(message, requestedSong);
-}
-// Queue status template
-const status = (queue) => `Volume: \`${queue.volume}%\` | Filter: \`${queue.filter || "Off"}\` | Loop: \`${queue.repeatMode ? queue.repeatMode == 2 ? "All Queue" : "This Song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
-
-
-distube
-    .on("playSong", (message, queue, song) => message.channel.send(
-        `Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}\n${status(queue)}`
-    ))
-    .on("addSong", (message, queue, song) => message.channel.send(
-        `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
-    ))
-    .on("playList", (message, queue, playlist, song) => message.channel.send(
-        `Play \`${playlist.name}\` playlist (${playlist.songs.length} songs).\nRequested by: ${song.user}\nNow playing \`${song.name}\` - \`${song.formattedDuration}\`\n${status(queue)}`
-    ))
-    .on("addList", (message, queue, playlist) => message.channel.send(
-        `Added \`${playlist.name}\` playlist (${playlist.songs.length} songs) to queue\n${status(queue)}`
-    ))
-    .on("error", (message, e) => {
-        console.error(e)
-        message.channel.send("An error encountered: " + e);
+            return [2 /*return*/];
+        }
+        console.log(cmd);
+        switch (cmd) {
+            case "p":
+                cmd = "play";
+                break;
+            case "s":
+                cmd = "skip";
+                break;
+            case "q":
+                cmd = "queue";
+                break;
+            case "ff":
+                cmd = "fastforward";
+                break;
+            case "rr":
+                cmd = "rewind";
+                break;
+        }
+        console.log(cmd);
+        PlayService[cmd](message, cmdString);
+        return [2 /*return*/];
     });
-
+}); });
+function checkPermissions(message) {
+    var voiceChannel = message.member.voice.channel;
+    if (!voiceChannel) {
+        message.channel.send("You need to be in a voice channel to play music!");
+        return false;
+    }
+    var permissions = voiceChannel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+        message.channel.send("I need the permissions to join and speak in your voice channel!");
+        return false;
+    }
+    return true;
+}
 client.login(token);
---
+++ src/index.ts
@@ -0,0 +1,83 @@
+const { acceptedMethods } = require("../src/configs/config.json")
+const Discord = require("discord.js")
+require("dotenv").config()
+const { default: PlayService } = require("./services/playService")
+const Distube = require("distube")
+
+const prefix = process.env.PREFIX
+const token = process.env.TOKEN
+
+const client = new Discord.Client()
+const distube = new Distube(client, {
+    searchSongs: false,
+    emitNewSongOnly: true,
+})
+PlayService.setDistube(distube)
+
+const queue = new Map()
+
+client.once("ready", () => {
+    console.log("Ready!")
+})
+
+client.once("reconnecting", () => {
+    console.log("Reconnecting!")
+})
+
+client.once("disconnect", () => {
+    console.log("Disconnect!")
+})
+
+client.on("message", async (message) => {
+    if (message.author.bot) return
+    if (!message.content.startsWith(prefix)) return
+    if (!checkPermissions(message)) return
+    
+    var args = message.content.split(" ")
+    var cmd = args[0].substr(1, args[0].length)
+    args = args.splice(1, args.length)
+    const cmdString = args.join(" ")
+
+    if (!acceptedMethods.includes(cmd)) {
+        message.channel.send("You need to enter a valid command!")
+        return
+    }
+
+    console.log(cmd)
+    switch (cmd) {
+        case "p":
+            cmd = "play"
+            break
+        case "s":
+            cmd = "skip"
+            break
+        case "q":
+            cmd = "queue"
+            break
+        case "ff":
+            cmd = "fastforward"
+            break
+        case "rr":
+            cmd = "rewind"
+            break
+    }
+    console.log(cmd)
+    PlayService[cmd](message, cmdString)
+})
+
+function checkPermissions(message) {
+    const voiceChannel = message.member.voice.channel
+    if (!voiceChannel) {
+        message.channel.send("You need to be in a voice channel to play music!")
+        return false
+    }
+    const permissions = voiceChannel.permissionsFor(message.client.user)
+    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+        message.channel.send(
+            "I need the permissions to join and speak in your voice channel!"
+        )
+        return false
+    }
+    return true
+}
+client.login(token)
